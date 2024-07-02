490424364b1ab732eb3f8d05c5ac8edb2f862f54
Benjamin Jarrell <benjaminejarrell@gmail.com>
2024-07-01 19:35:14 -0500

    add output

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,8 @@ output "entraid_tenet_id" {
   description = "EntraID Tenet ID"
   value       = local.azuread_tenet_id
 }
+
+output "oidc_issuer" {
+  description = "OIDC Issuer URL"
+  value       = "https://login.microsoftonline.com/${local.azuread_tenet_id}/v2.0"
+}
